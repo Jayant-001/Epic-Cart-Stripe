79931501b4f974eb45a242bbe7267cd64df39a99
Jayant-001 <jayant.gupta.dln@gmail.com>
2023-11-07 12:08:38 +0530

    app ss uploadg

--- src/components/cart/ClearCartProducts.js
+++ src/components/cart/ClearCartProducts.js
@@ -3,30 +3,34 @@
 import { MdDone } from "react-icons/md";
 import { useEffect, useState } from "react";
 import { useShoppingCart } from "use-shopping-cart";
+import Lottie from "lottie-react";
+import delivery from "@/delivery.json";
+import Link from "next/link";
 
 const ClearCartProducts = ({ email }) => {
     const { clearCart } = useShoppingCart();
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-
         const deletee = () => {
-            setLoading(true)
+            setLoading(true);
             clearCart();
-        }
+        };
 
-            if(loading === false) 
-        deletee();
-    }, [])
-    
+        if (loading === false) deletee();
+    }, []);
 
     return (
         <div className="py-4 px-8 space-y-4 rounded-md max-w-lg mx-auto">
-            <MdDone className="w-24 h-24 mx-auto flex-shrink-0 text-lime-600" />
             <h2 className="text-4xl font-semibold flex flex-col items-center space-x-1">
                 Your order has been confirmed
             </h2>
+            <Link className="text-blue-500 hover:underline text-lg" href="/">Go Home</Link>
             <p className="text-lg">For envoice check email - {email}</p>
+            {/* <MdDone className="w-24 h-24 mx-auto flex-shrink-0 text-lime-600" /> */}
+            <div className="w-auto h-auto ">
+                <Lottie style={{objectFit: 'fill'}} animationData={delivery} height={10} width={"100%"} />
+            </div>
         </div>
     );
 };
